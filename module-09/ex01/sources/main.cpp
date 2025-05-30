0b71215a92643d0721aea59eccf7ed13d3853631
codingfrog27 <codingmerlin@gmail.com>
2025-05-30 17:43:27 +0200

    ex 01 cleaned up and i think eval ready?

--- module-09/ex01/sources/main.cpp
+++ module-09/ex01/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mde-cloe <mde-cloe@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/17 13:36:32 by mde-cloe      #+#    #+#                 */
-/*   Updated: 2025/03/31 18:44:34 by mde-cloe      ########   odam.nl         */
+/*   Updated: 2025/05/30 14:52:37 by mde-cloe      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,6 @@
 #define LINE "-----------------------------------------"
 
 
-
-void	promptEnter()
-{	
-	std::cout << C_YELLOW "\nPress Enter to continue..." C_RESET << std::endl;
-	std::cin.get();
-}
-
-int RandomNbr()
-{
-	static std::mt19937 generator(std::time(nullptr));
-	static std::uniform_int_distribution<int> distribution(0, 100);
-		
-	return distribution(generator);
-}
 
 
 void	InputIsValid(std::string input)
@@ -72,31 +58,37 @@ void	calculate(int op, std::stack<float> &stack)
 		result = b * a;
 		break;
 	default: 
-		std::cerr << "WHATTTTEHIUAFHIUHFIUHEF\n\n" << std::endl; //should never reach but test
-		break;
+		throw (std::invalid_argument("Invalid operator that somehow got through regex"));
 	}
 	stack.push(result);
 }
 
 void	Tokenise(std::string	&input)
 {
-	std::regex	nbrs("^\\d+$");
-	std::regex	ops("^(\\+|-|\\*|/)$");
+	int					nbrCount = 0;
+	std::regex			nbrPattern("^\\d+$");
+	std::regex			opPattern("^(\\+|-|\\*|/)$");
 	std::stack<float>	mathStack;
 	std::istringstream	iss(input);
 	std::string			current;
-	// float				nbr;
+
 	while (iss >> current)
 	{
-		if (std::regex_match(current, nbrs))
-			mathStack.push(stof(current)); //shouldnt throw but test
-		else if (std::regex_match(current, ops))
+		if (std::regex_match(current, nbrPattern))
+		{
+			if (++nbrCount > 10)
+				throw (std::invalid_argument("no more than 10 nunbers allowed!"));
+			mathStack.push(stoi(current)); 
+		}
+		else if (std::regex_match(current, opPattern))
 			calculate(current[0], mathStack);
 		else 
 			throw (std::invalid_argument("Invalid token detected"));
 	}
 	if (mathStack.size() > 1)
 		throw (std::invalid_argument("too many numbers remaining"));
+
+	
 	std::cout << mathStack.top() << std::endl;
 }
 
@@ -105,13 +97,17 @@ int main(int argc, char **argv) {
 		
 	if (argc != 2) {
 		std::cerr << C_RED \
-		"Error: Please give one and only one argument to RPN" C_RESET << std::endl;
+		"Error:" C_RESET "Please give one and only one argument to RPN" << std::endl;
 	}
 	std::string input = argv[1];
 	try
 	{
 		InputIsValid(input);
 		Tokenise(input);
+	}
+	catch (const std::out_of_range &e)
+	{
+		std::cerr << C_RED "Error: " C_RESET "Number too large to be contained as int" << '\n';
 	}
 	catch(const std::exception& e)
 	{
@@ -124,9 +120,6 @@ int main(int argc, char **argv) {
 }
 
 
-//wtf absolutely speedran this exercise pog??
-
-//mostly cleanup, nicer func split and use class if i want, test some moer AND FINISH BBY
 
 
 
@@ -137,7 +130,6 @@ int main(int argc, char **argv) {
 // handle + - / and *
 // no brackets or decimal handling needed
 
-//parsed into abstract syntax trees??
 
 //OR use a stack and as you read keep popping in numbers untill you find operator (+, - etc.)
 // then if found pop last 2 operands from stack (2nd popped becomes first in calc), 
